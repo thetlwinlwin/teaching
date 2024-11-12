fb2c9a49d2032a5e6042d8fa3c882f384ecdbf0e
Thet Lwin Lwin <minthantlwin8@gmail.com>
2024-11-12 20:28:41 +0630

    delete old carriculum

--- vincent/simple_program.py
+++
@@ -1,7 +0,0 @@
-username = input('What is your name : ')
-gender = input('Your Gender (M/F) : ').lower()
-
-result = 'Mr.'+username if gender == 'm' else 'Miss '+username
-
-print(f'Hello! {result}!')
-
