c6f806ea2ca80d08383236c852fa1601f8b030bf
Thet Lwin Lwin <minthantlwin8@gmail.com>
2024-11-18 21:29:39 +0630

    22_19_mj(bubu) finished

--- bubu/code_22_19_mj.py
+++ bubu/code_22_19_mj.py
@@ -1,40 +1,70 @@
 ITEM_NUMBER = [i for i in range(10)]
-ITEM_DESCRIPTION = ["Item "+str(i) for i in range(10)]
+ITEM_DESCRIPTION = ["Item " + str(i) for i in range(10)]
 RESERVE_PRICE = [3706, 6119, 4209, 8104, 332, 6648, 1502, 7086, 4772, 4744]
-num_of_bid  = [0 for _ in range(10)]
+num_of_bids = [0 for _ in range(10)]
 
-cur_highest_price = [3706, 6119, 4209, 8104, 332, 6648, 1502, 7086, 4772, 4744]
-highest_price_by = [None for _ in range(10)]
+cur_highest_price = [0 for _ in range(10)]
+is_sold = [False for _ in range(10)]
+num_of_item_sold = 0
 
-print('WELCOME')
+is_auction_end = False
+print("WELCOME")
 
-for _ in range(3):
+while not is_auction_end:
     # ask Item Number
-    item_num = int(input("Search Item by ID : "))
+    item_num = input("Search Item by ID (press q to quit) : ")
+    if item_num == "q" or item_num == "Q":
+        is_auction_end = True
+    else:
+        item_num = int(item_num)
+        if is_sold[item_num]:
+            print("*" * 50)
+            print("That item", item_num, "is sold out!")
+            print("*" * 50)
+        else:
+            # Print out the item data
+            print("*" * 50)
+            print("Item Number : " + str(item_num))
+            print("Description : " + ITEM_DESCRIPTION[item_num])
+            print("Current Highest : " + str(cur_highest_price[item_num]))
+            print("Number of Bids :", num_of_bids[item_num])
+            print("*" * 50)
 
-    # Print out the item data
-    print("*"*50)
-    print('Item Number : '+str(item_num))
-    print('Description : '+ITEM_DESCRIPTION[item_num])
-    print('Current Highest : '+str(cur_highest_price[item_num]))
-    print("*"*50)
+            # bidding
+            buyer_num = int(input("Enter your Buyer number : "))
+            bid_amount = float(input("Enter the amount you want to bid : "))
 
-    # bidding
-    buyer_num = int(input('Enter your number : '))
-    bid_amount = float(input('Enter the amount you want to bid : '))
+            while bid_amount < cur_highest_price[item_num]:
+                print("too low")
+                bid_amount = float(input("Enter the amount you want to bid : "))
 
-    while bid_amount < cur_highest_price[item_num]:
-        print('too low')
-        bid_amount = float(input('Enter the amount you want to bid : '))
+            cur_highest_price[item_num] = bid_amount  # type: ignore
+            num_of_bids[item_num] += 1
 
-    cur_highest_price[item_num] = bid_amount
-    highest_price_by[item_num] = buyer_num
+            if bid_amount >= RESERVE_PRICE[item_num]:
+                is_sold[item_num] = True
+                num_of_item_sold += 1
+                print("\n", "*" * 50)
+                print("Congrat!")
+                print("Sold to", buyer_num, "by amount of", bid_amount * 1.1)
+                print("\n", "*" * 50)
+    print("current highest is ", cur_highest_price)
+    print("\n\n")
 
-    print('current highest price')
-    print(cur_highest_price)
-    print('highest price by')
-    print(highest_price_by)
-    
+num_of_items_not_meeting_price = 0
+num_of_items_not_bid_once = 0
+for item_num in ITEM_NUMBER:
+    if is_sold[item_num] == False:
+        print("Item", item_num, "got", cur_highest_price[item_num])
+        num_of_items_not_meeting_price += 1
 
+print("\n")
+for item_num in ITEM_NUMBER:
+    if num_of_bids[item_num] == 0:
+        print("Item", item_num, "received no bidding")
+        num_of_items_not_bid_once += 1
 
-
+print("\n")
+print("total item sold", num_of_item_sold)
+print("Num of Item not meeting the price", num_of_items_not_meeting_price)
+print("Num of Item not bid once", num_of_items_not_bid_once)
