11cb38b6ecdb1c418067c60b68c7f83e9ce1b359
Thet Lwin Lwin <minthantlwin8@gmail.com>
2025-02-01 14:19:55 +0630

    vincent

---
+++ vincent/exercises/password_generator.py
@@ -0,0 +1,23 @@
+import string
+import random
+
+length = int(input("Enter the password length : "))
+
+while length < 8:
+    print("Password should be at least 6 characters long.")
+    length = int(input("Enter the password length : "))
+
+password = ""
+
+for _ in range(length):
+    char_list = [
+        string.ascii_uppercase,
+        string.ascii_lowercase,
+        string.digits,
+        string.punctuation,
+    ]
+    pick = random.choice(char_list)
+    char = random.choice(pick)
+    password += char
+
+print("Your password is :", password)
---
+++ vincent/exercises/to_do_list.py
@@ -0,0 +1,10 @@
+to_do_list = []
+
+is_finished = False
+
+while not is_finished:
+    to_do = input("Enter (type 'done' to stop) : ").capitalize()
+    if to_do == "Done":
+        is_finished = True
+    elif to_do not in to_do_list:
+        to_do_list.append(to_do)
